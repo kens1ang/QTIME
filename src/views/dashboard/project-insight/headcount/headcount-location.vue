6aee8dd31d08fd09b822f13c053f7ac3dd677c45
Lai Ken Siang <ken0815b@gmail.com>
2024-08-14 11:39:48 +0800

    refine location by headcount

--- src/views/dashboard/project-insight/headcount/headcount-location.vue
+++ src/views/dashboard/project-insight/headcount/headcount-location.vue
@@ -51,104 +51,92 @@ export default {
     </li>
   </ul>
 
-  <!-- Attendance -->
-  <div v-if="currentTab === 'attendance'">
-    <div class="pin carpark" @click="toggleCarParkDetails">
-      <span>Car Park: 9</span>
-    </div>
-
-    <div v-if="showCarParkDetails">
-      <div class="pin carpark-level-3">
-        <span>Level 3: 7</span>
-      </div>
-      <div class="pin carpark-level-2">
-        <span>Level 2: 2</span>
-      </div>
-    </div>
-
-    <div class="pin podium" @click="togglePodiumDetails">
-      <span>Podium: 7</span>
-    </div>
-
-    <div v-if="showPodiumDetails">
-      <div class="pin podium-ground-floor">
-        <span>Ground Floor: 6</span>
-      </div>
-      <div class="pin podium-level-5">
-        <span>Level 5: 1</span>
-      </div>
-    </div>
-
-    <div class="pin building" @click="toggleBuildingDetails">
-      <span>Building: 9</span>
-    </div>
-
-    <div v-if="showBuildingDetails">
-      <div class="pin building-level-9">
-        <span>Level 9: 2</span>
-      </div>
-      <div class="pin building-level-8">
-        <span>Level 8: 1</span>
-      </div>
-      <div class="pin building-level-6">
-        <span>Level 6: 2</span>
-      </div>
-      <div class="pin building-level-12">
-        <span>Level 12: 3</span>
-      </div>
-      <div class="pin building-level-14">
-        <span>Level 14: 1</span>
-      </div>
-    </div>
-
-    <div class="pin office">
-      <span>Office: 10</span>
-    </div>
-  </div>
-
-  <!-- RFID -->
-  <div v-if="currentTab === 'rfid'">
-    <div class="pin rfid1">
-      <span>RFID 1</span>
-    </div>
-    <div class="pin rfid2">
-      <span>RFID 2</span>
-    </div>
-    <div class="pin rfid3">
-      <span>RFID 3</span>
-    </div>
-    <div class="pin rfid4">
-      <span>RFID 4</span>
-    </div>
-    <div class="pin rfid5">
-      <span>RFID 5</span>
-    </div>
-    <div class="pin rfid6">
-      <span>RFID 6</span>
-    </div>
-    <div class="pin rfid7">
-      <span>RFID 7</span>
-    </div>
-    <div class="pin rfid8">
-      <span>RFID 8</span>
-    </div>
-    <div class="pin rfid9">
-      <span>RFID 9</span>
-    </div>
-  </div>
-
   <!-- Site img & table -->
   <BRow>
+    <!-- Site Image -->
     <BCol xl="9" class="pe-0">
+      <!-- Attendance -->
+      <div v-if="currentTab === 'attendance'">
+        <div class="pin carpark" @click="toggleCarParkDetails">
+          <span>Car Park: 9</span>
+        </div>
+
+        <div v-if="showCarParkDetails">
+          <div class="pin carpark-level-3">
+            <span>Level 3: 7</span>
+          </div>
+          <div class="pin carpark-level-2">
+            <span>Level 2: 2</span>
+          </div>
+        </div>
+
+        <div class="pin podium" @click="togglePodiumDetails">
+          <span>Podium: 7</span>
+        </div>
+
+        <div v-if="showPodiumDetails">
+          <div class="pin podium-ground-floor">
+            <span>Ground Floor: 6</span>
+          </div>
+          <div class="pin podium-level-5">
+            <span>Level 5: 1</span>
+          </div>
+        </div>
+
+        <div class="pin building">
+          <span>Building: 9</span>
+        </div>
+
+        <div class="pin office">
+          <span>Office: 10</span>
+        </div>
+      </div>
+
+      <!-- RFID -->
+      <div v-if="currentTab === 'rfid'">
+        <div class="pin rfid1">
+          <span>RFID 1</span>
+        </div>
+        <div class="pin rfid2">
+          <span>RFID 2</span>
+        </div>
+        <div class="pin rfid3">
+          <span>RFID 3</span>
+        </div>
+        <div class="pin rfid4">
+          <span>RFID 4</span>
+        </div>
+        <div class="pin rfid5">
+          <span>RFID 5</span>
+        </div>
+        <div class="pin rfid6">
+          <span>RFID 6</span>
+        </div>
+        <div class="pin rfid7">
+          <span>RFID 7</span>
+        </div>
+        <div class="pin rfid8">
+          <span>RFID 8</span>
+        </div>
+        <div class="pin rfid9">
+          <span>RFID 9</span>
+        </div>
+      </div>
+
       <img
         src="../../../../assets/images/site.jpg"
         alt="Responsive image"
         class="img-fluid site-img"
       />
     </BCol>
+
+    <!-- Table -->
     <BCol xl="3" class="ps-0">
       <BCardBody>
-        <div class="table-responsive table-card overflow-auto" style="max-height: 500px;">
+        <div
+          class="table-responsive table-card overflow-auto"
+          style="max-height: 500px"
+        >
           <table
             class="table align-middle table-borderless table-centered table-nowrap mb-0"
           >
@@ -252,8 +240,18 @@ export default {
 }
 
 .carpark {
-  top: 35%;
+  top: 20%;
   left: 15%;
+}
+
+.carpark-level-3 {
+  top: 10%;
+  left: 5%;
+}
+
+.carpark-level-2 {
+  top: 30%;
+  left: 5%;
 }
 
 .building {
@@ -262,7 +260,17 @@ export default {
 }
 
 .podium {
-  top: 45%;
+  top: 35%;
+  left: 60%;
+}
+
+.podium-ground-floor {
+  top: 25%;
+  left: 50%;
+}
+
+.podium-level-5 {
+  top: 40%;
   left: 50%;
 }
 
@@ -313,51 +321,6 @@ export default {
 
 .rfid9 {
   top: 45%;
-  left: 70%;
-}
-
-.carpark-level-3 {
-  top: 30%;
-  left: 10%;
-}
-
-.carpark-level-2 {
-  top: 40%;
-  left: 10%;
-}
-
-.podium-ground-floor {
-  top: 50%;
-  left: 40%;
-}
-
-.podium-level-5 {
-  top: 40%;
-  left: 40%;
-}
-
-.building-level-9 {
-  top: 90%;
-  left: 50%;
-}
-
-.building-level-8 {
-  top: 90%;
-  left: 60%;
-}
-
-.building-level-6 {
-  top: 90%;
-  left: 40%;
-}
-
-.building-level-12 {
-  top: 80%;
-  left: 40%;
-}
-
-.building-level-14 {
-  top: 90%;
   left: 70%;
 }
 
